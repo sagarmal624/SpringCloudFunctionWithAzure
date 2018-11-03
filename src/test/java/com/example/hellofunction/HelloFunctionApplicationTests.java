683dc36e94d9b817950ef4a349c443b5c8ba7a99
Toshiaki Maki <tmaki@pivotal.io>
2018-11-03 15:00:42 +0900

    Add test

--- src/test/java/com/example/hellofunction/HelloFunctionApplicationTests.java
+++ src/test/java/com/example/hellofunction/HelloFunctionApplicationTests.java
@@ -2,15 +2,23 @@ package com.example.hellofunction;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloFunctionApplicationTests {
+	@Autowired
+	TestRestTemplate restTemplate;
 
-    @Test
-    public void contextLoads() {
-    }
-
+	@Test
+	public void contextLoads() {
+		String response = restTemplate.postForObject("/hello", "making", String.class);
+		assertThat(response).isEqualTo("Hello making!");
+	}
 }
