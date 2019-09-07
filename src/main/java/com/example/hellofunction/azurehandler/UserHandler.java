5c6efff89b6061e7a278b54405a4fefa50b7188c
Sagar Shankhala <sagshank@publicisgroupe.net>
2019-09-07 11:56:56 +0530

    added crud operations

---
+++ src/main/java/com/example/hellofunction/azurehandler/UserHandler.java
@@ -0,0 +1,45 @@
+package com.example.hellofunction.azurehandler;
+
+import com.example.hellofunction.domains.User;
+import com.example.hellofunction.util.ResponseDTO;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.BindingName;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+import org.springframework.cloud.function.adapter.azure.AzureSpringBootRequestHandler;
+
+import java.util.Optional;
+
+public class UserHandler extends AzureSpringBootRequestHandler<Object, ResponseDTO> {
+
+    @FunctionName("userSave")
+    public ResponseDTO save(
+            @HttpTrigger(name = "userSaveRequest", methods = {HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<User>> request,
+            ExecutionContext context) {
+        return handleRequest(request.getBody().get(), context);
+    }
+
+    @FunctionName("userGet")
+    public ResponseDTO get(
+            @HttpTrigger(name = "userGetRequest", methods = {HttpMethod.GET}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage request,
+            @BindingName("id") Integer id, ExecutionContext context) {
+        return handleRequest(id, context);
+    }
+
+    @FunctionName("userDelete")
+    public ResponseDTO delete(
+            @HttpTrigger(name = "userDeleteRequest", methods = {HttpMethod.DELETE}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage request,
+            @BindingName("id") Integer id, ExecutionContext context) {
+        return handleRequest(id, context);
+    }
+
+    @FunctionName("userUpdate")
+    public ResponseDTO update(
+            @HttpTrigger(name = "userUpdateRequest", methods = {HttpMethod.PUT}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<User>> request,
+            ExecutionContext context) {
+        return handleRequest(request.getBody().get(), context);
+    }
+}
---
+++ src/main/java/com/example/hellofunction/util/ResponseDTO.java
@@ -0,0 +1,31 @@
+package com.example.hellofunction.util;
+
+public class ResponseDTO {
+    private String message;
+    private boolean status;
+    private Object data;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
