4faee5b5c83dc3262554dfe0b31d122a2011bf2c
Sagar Shankhala <sagshank@publicisgroupe.net>
2019-09-07 11:59:20 +0530

    change name of main file

--- src/main/java/com/example/hellofunction/SpringCloudFunctionDemoApplication.java
+++ src/main/java/com/example/hellofunction/SpringCloudFunctionDemoApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloFunctionApplication {
+public class SpringCloudFunctionDemoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HelloFunctionApplication.class, args);
+        SpringApplication.run(SpringCloudFunctionDemoApplication.class, args);
     }
 }
